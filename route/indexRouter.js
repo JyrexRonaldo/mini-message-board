e1d1cfa638fade7241b3373a2f72b69e92622a51
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-01-26 16:50:21 +0000

    Add page for displaying individual message details

--- route/indexRouter.js
+++ route/indexRouter.js
@@ -6,5 +6,8 @@ const indexRouter = Router();
 indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages });
 });
+indexRouter.get("/message/:number", (req,res) => {
+  res.render("message", {message: messages[Number(req.params.number)]})
+})
 
 module.exports = indexRouter;
